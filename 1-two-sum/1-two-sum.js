3a8d7d5396e10d3de21d8bbb6b7acf2ea0538378
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-07-20 19:32:22 +0530

    Time: 113 ms (58.19%), Space: 45.3 MB (6.98%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,14 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let map = {};
     let newArr = [];
     for(let i=0; i<nums.length; i++){
-        for(let j=i +1; j<nums.length; j++){
-            if(nums[i]+nums[j]===target){
-                newArr[0] = i;
-                newArr[1] = j;
+        let rem = target-nums[i];        
+        if(!map.hasOwnProperty(rem)){
+                map[nums[i]]=i;
+            } else {
+                newArr[0]=i;
+                newArr[1]=map[rem];
             }
-        }     
     }
     return newArr;
 };
