a5f968fb1ad0e2d41ce0c8ed30986d41dbc1f91d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-07-23 09:36:07 +0000

    Sync LeetCode submission - Implement Queue using Stacks (javascript)

---
+++ problems/implement_queue_using_stacks/solution.js
@@ -0,0 +1,45 @@
+
+var MyQueue = function() {
+    this.arr = [];
+    
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.arr.push(x);
+    
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    const num = this.arr.shift();
+    return num;
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.arr[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.arr.length===0 ? true : false;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
