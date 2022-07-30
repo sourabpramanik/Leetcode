2620f5342d98ef04d4e74902bc4773fbda86641a
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-07-30 16:09:02 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

--- problems/contains_duplicate/solution.js
+++ problems/contains_duplicate/solution.js
@@ -3,12 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for(let i = 0; i < nums.length; i++){            
-        for(let j = i+1; j < nums.length; j++){     
-            if(nums[i] == nums[j]){
-                return true;
-            }            
-        }
-    }
-    return false
+    return nums.length !== [...new Set(nums)].length
 };
