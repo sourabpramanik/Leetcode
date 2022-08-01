6db576392eeb086380055f9a8b5ee2e66587643a
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-01 14:16:33 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -4,16 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = {};
-    let arr = [];
-    
+
     for(let i=0; i<nums.length; i++){
-        let rem = target - nums[i];
-        if(!map[rem]){
-            map[nums[i]] = i+1;
-        } else{
-            arr.push(i, map[rem]-1)
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]+nums[j]===target){
+                return [i, j]
+            }
         }
     }
-    return arr
+    return []
 };
