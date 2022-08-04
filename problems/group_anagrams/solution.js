9dddaee73faeccea10eae3c44193a5f1c934f353
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-04 14:38:28 +0000

    Sync LeetCode submission - Group Anagrams (javascript)

---
+++ problems/group_anagrams/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {};
+ 
+    for(let str of strs){
+        let sortedStr =  [...str].sort().join("")
+        if(!map[sortedStr]){
+            map[sortedStr] = []
+        } 
+        map[sortedStr].push(str)
+    }
+    return Object.values(map)
+};
