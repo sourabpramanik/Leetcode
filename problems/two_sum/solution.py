6854dbbb965672764b988224552c541885968353
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-07 11:30:54 +0000

    Sync LeetCode submission - Two Sum (python3)

---
+++ problems/two_sum/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return [i, d[nums[i]]]
+            else:
+                d[target - nums[i]] = i
