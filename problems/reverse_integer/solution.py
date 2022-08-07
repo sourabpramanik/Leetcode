0e64ee0726724da3581415af5f2a97fd73ac4b0a
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-07 13:53:17 +0000

    Sync LeetCode submission - Reverse Integer (python3)

---
+++ problems/reverse_integer/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def reverse(self, x: int) -> int:        
+        num = int(str(abs(x))[::-1])
+        if( num > 0x7FFFFFFF ):  return 0
+        return num*-1 if x<0 else num
