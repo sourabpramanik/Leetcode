483d99d0f4b4185933646ef61d617201a6b2300c
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-09 10:25:12 +0530

    Time: 67 ms (86.67%), Space: 42 MB (68.86%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -12,14 +12,19 @@
  */
 var inorderTraversal = function(root) {
     let arr = []
-    const helper = (node) => {
-        if(node==null) return
-    
-        helper(node.left)
-        arr.push(node.val)
-        helper(node.right)
+    let stack = []
+    let current = root
+    while(current || stack.length>0){
+        while(current){
+            stack.push(current)
+            current = current.left
+        }
+        current = stack.pop()
+        arr.push(current.val)
+        current = current.right
     }
-    helper(root)
+    
+  
     return arr
     
 };
