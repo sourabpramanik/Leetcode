ca6441bc77af7a353857f6d59e0a73bdf20c4dd3
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-18 08:31:31 +0530

    Time: 119 ms (43.17%), Space: 14.4 MB (25.56%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -5,21 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    prev, first, mid, last = None, None, None, None
+    prev, first, last = None, None, None
     
     def inOrder(self,node):
         if node is None: return None
         
         self.inOrder(node.left)
         
-        if(self.prev and node.val < self.prev.val):
-            
-            if self.first is None:
-                self.first = self.prev
-                self.mid = node
-            
-            else:
-                self.last = node
+        if(self.first is None and node.val < self.prev.val):
+            self.first = self.prev                
+        
+        if(self.first and node.val < self.prev.val):
+            self.last = node
+                
         
         self.prev = node
         self.inOrder(node.right)
@@ -27,12 +25,8 @@ class Solution:
         
         self.prev = TreeNode(float("-inf"))
         self.inOrder(root)
-        if(self.first and self.last):
-            t = self.first.val
-            self.first.val = self.last.val
-            self.last.val = t
-        elif(self.first and self.mid):
-            t = self.first.val
-            self.first.val = self.mid.val
-            self.mid.val = t
+        
+        t = self.first.val
+        self.first.val = self.last.val
+        self.last.val = t
         
