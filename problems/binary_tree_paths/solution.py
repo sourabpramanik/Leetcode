d6cb1d35641b963e92cf8c5a27454371f24d36d8
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-20 04:25:45 +0000

    Sync LeetCode submission - Binary Tree Paths (python3)

---
+++ problems/binary_tree_paths/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        res = []
+      
+        
+        def rec(node, ls):
+            if node is None:
+                return res
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.left:
+                rec(node.left, ls+str(node.val)+"->")
+            if node.right:
+                rec(node.right, ls+str(node.val)+"->")
+        
+        rec(root, "")
+        
+        return res
+            
