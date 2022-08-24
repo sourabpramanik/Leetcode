482b3cb0af2b70aafa09bbdcafe26d51733479b3
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-24 07:20:28 +0530

    Time: 147 ms (20.46%), Space: 50.3 MB (58.89%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum = -Infinity
+    let currSum=0
+    for(let i=0; i<nums.length; i++){
+        currSum += nums[i]
+        if(maxSum<currSum){
+            maxSum=currSum
+        }
+        if(currSum<0){
+            currSum = 0
+        }
+    }
+    
+    return maxSum
+};
