ef231d0c076922267a0dba4e90d17047cf7efd8c
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-24 01:04:41 +0000

    Sync LeetCode submission - Sort Colors (javascript)

--- problems/sort_colors/solution.js
+++ problems/sort_colors/solution.js
@@ -3,16 +3,42 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    for(let i=1; i<nums.length; i++){
-        let curr = nums[i];
-        
-        let j=i-1;
-        
-        while(j>-1 && curr < nums[j]){
-            nums[j+1] = nums[j]
-            j--;
-        }
-        nums[j+1] = curr;
+    if(nums.length===1){
+        return nums
     }
+    
+    let mid = Math.floor(nums.length/2)
         
+    let L = nums.slice(0,mid)
+    let R = nums.slice(mid)
+    sortColors(L)
+    sortColors(R)
+    
+    let i=0, j=0, k=0
+    
+    while(i<L.length && j<R.length){
+        if(L[i]<R[j]){
+            nums[k] = L[i]
+            i++
+        }else{
+            nums[k] = R[j]
+            j++
+        }
+        k++
+    }
+    
+    while(i<L.length){
+        nums[k] = L[i]
+        i++
+        k++
+    }
+    
+    while(j<R.length){
+        nums[k] = R[j]
+        j++
+        k++
+    }
+    
+    return nums
+    
 };
