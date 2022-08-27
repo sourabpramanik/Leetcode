76012f3bb7b58ecf63d62c6822ec525f643179e3
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-08-27 16:26:21 +0530

    Time: 153 ms (71.41%), Space: 15.4 MB (34.81%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -10,7 +10,7 @@ class Solution:
                 obj[v]=1
         
         for v in obj:
-            if(obj[v]>len(nums)/3):
+            if(obj[v]>floor(len(nums)/3)):
                 res.append(v)
         
         return res
