1f80901a8b61fd20a97be854cfd80f04743a930d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-09-07 03:42:17 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

---
+++ problems/climbing_stairs/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def __init__(self):
+        self.dic = {1:1, 2:2}
+        
+    def climbStairs(self, n: int) -> int:
+        if n not in self.dic:
+            self.dic[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
+        
+        return self.dic[n]
+    
+
