0ed358c5673e92eb42b083f87afe513d5e3a6dbb
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-09-27 23:04:20 +0530

    Time: 79 ms (21.03%), Space: 14.2 MB (10.27%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,18 +1,14 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = []        
+        ans = []
         nums.sort()
-        
-        def rec(i, ds):
+        def rec(i, ds):        
             ans.append(ds[:])
-            
             for j in range(i, len(nums)):
-                if(j>i and nums[j]==nums[j-1]):
+                if j>i and nums[j]==nums[j-1]:
                     continue
-                
                 ds.append(nums[j])
                 rec(j+1, ds)
                 ds.pop(len(ds)-1)
         rec(0, [])
         return ans
-            
