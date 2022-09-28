d717f3a3a0d36240e579e5bce00314394f919f0c
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-09-28 16:33:12 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (python3)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -22,10 +22,10 @@ class Solution:
         while(s<=e):
             mid=s+(e-s)//2
             
-            if(nums[mid]>=target):
-                e=mid-1  
-            else:
+            if(nums[mid]<target):
                 s=mid+1
+            else:
+                e=mid-1
             
             if(nums[mid]==target):
                 ans[0]=mid
