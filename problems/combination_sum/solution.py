a997a639d44fe1d5575b43353ddd42441fadb35a
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-09-30 02:18:42 +0000

    Sync LeetCode submission - Combination Sum (python3)

---
+++ problems/combination_sum/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, A, tar) -> List[List[int]]:
+        
+        ans = []
+        def rec(i, ds, rem):
+            if rem==0:
+                ans.append(ds[:])
+                return
+            
+            if i>=len(A): return
+            
+            if rem>=A[i]:
+                ds.append(A[i])
+                rem-=A[i]
+                rec(i, ds, rem)
+                rem+=A[i]
+                ds.pop(len(ds)-1)
+                
+            rec(i+1, ds, rem)
+            
+        
+        rec(0, [], tar)
+        
+        return ans
