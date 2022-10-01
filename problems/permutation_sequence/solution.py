0872b091c9b353f3c3544fe9cbee996bea233302
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-01 11:06:07 +0000

    Sync LeetCode submission - Permutation Sequence (python3)

--- problems/permutation_sequence/solution.py
+++ problems/permutation_sequence/solution.py
@@ -1,22 +1,26 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         ans=""
-        ds=[]
+        
         fact=1
-        
+        nums = []
         for i in range(1, n):
-            ds.append(i)
-            fact = fact*i
+            fact *= i
+            nums.append(i)
         
-        ds.append(n)
-        k=k-1
+        nums.append(n)
+        k-=1
         
         while True:
-            ans = ans + str(ds[k//fact])
-            ds.pop(k//fact)
-            if len(ds)==0:
+            
+            ans = ans + str(nums[k//fact])
+            nums.pop(k//fact)
+            
+            if len(nums)==0:
                 break
+                
             k=k%fact
-            fact = fact//len(ds)
+            fact=fact//len(nums)
         
         return ans
+            
