542bd07e173dba6be7abf9d127ddcc8327d6006d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-16 07:45:04 +0530

    Time: 248 ms (26.04%), Space: 17.2 MB (89.01%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        enum = {}
+        for word in strs:
+            a = "".join(sorted(word))
+            
+            if a in enum:
+                enum[a].append(word)
+            else:
+                enum[a] = [word]
+        ans = []
+        for v in enum:
+            ans.append(enum[v])
+        return ans
+        
+        
