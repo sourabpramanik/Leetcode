e6164bff5ba141e7e04e560e13f46d84b0d743ab
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-16 09:05:29 +0530

    Time: 1050 ms (10.85%), Space: 17.7 MB (98.93%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -6,55 +6,29 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        tree = []
+        st1 = []
+        st2 = []
         
+        res = []
         
-        self.preOrder(root1, tree)
-        self.preOrder(root2, tree)
-        
-        return self.mergeSort(tree)
-    
-    def preOrder(self, node, arr):
-        if not node:
-            return None
-        
-        arr.append(node.val)
-        self.preOrder(node.left, arr)
-        self.preOrder(node.right, arr)
-                
-    
-    def mergeSort(self, arr):
-        if len(arr)>1:            
-        
-            mid = len(arr)//2
-
-            L = self.mergeSort(arr[:mid])
-            R = self.mergeSort(arr[mid:])
-
-
-            i=0
-            j=0
-            k=0
-
-            while i<len(L) and j<len(R):
-
-                if L[i]<=R[j]:
-                    arr[k] = L[i]
-                    i+=1
-                    k+=1
-                else:
-                    arr[k] = R[j]
-                    j+=1
-                    k+=1
-
-            while i<len(L):
-                arr[k] = L[i]
-                i+=1
-                k+=1
-
-            while j<len(R):
-                arr[k] = R[j]
-                j+=1
-                k+=1
+        while root1 or root2 or len(st1)>0 or len(st2)>0:
             
-        return arr
+            while root1:                
+                st1.append(root1)
+                root1 = root1.left
+            
+            while root2:
+                st2.append(root2)
+                root2 = root2.left
+            
+            if len(st2)==0 or len(st1)>0 and st1[-1].val<=st2[-1].val:
+                node1 = st1.pop()
+                res.append(node1.val)
+                root1 = node1.right
+            else:
+                node2 = st2.pop()
+                res.append(node2.val)
+                root2 = node2.right
+        
+        return res
+                
