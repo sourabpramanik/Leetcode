a5b7a5c54c86a21e423a7ce424558d4c6a6ec1b6
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-18 09:11:24 +0530

    Time: 259 ms (21.67%), Space: 18.7 MB (71.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,11 +5,24 @@ class Solution:
         for v in nums:
             m[v] = m.get(v, 0) + 1
         
+        bucket = [0]* (len(nums)+1)
+       
+        
+        for k in m:
+            v = m[k]
+            if bucket[v]==0:
+                bucket[v] = []
+            bucket[v].append(k)
+        
         res = []
         
-        for k, v in sorted(m.items(), key=lambda x:x[1], reverse=True):
-            res.append(k)
-            t-=1
-            if t==0:
-                return res
+        for i in range(len(bucket)-1, -1, -1):
+            arr = bucket[i]
+            if arr!=0:
+                for v in arr:
+                    res.append(v)
+                    t-=1
+                    if t==0:
+                        return res
+        
         
