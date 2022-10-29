418da6991a5cd11f7ff697c59e805085ceefd38a
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-29 05:42:39 +0000

    Sync LeetCode submission - Power of Two (python3)

--- problems/power_of_two/solution.py
+++ problems/power_of_two/solution.py
@@ -1,8 +1,5 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        
         if n==0:
             return False
-        
-        return n==1 or (n%2==0 and self.isPowerOfTwo(n//2))
-        
+        return True if n&n-1==0 else False
