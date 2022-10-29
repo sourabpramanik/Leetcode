4d9592e1882b850aa7f987eee6f617f70c25978b
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-29 11:14:50 +0000

    Sync LeetCode submission - Subsets (python3)

--- problems/subsets/solution.py
+++ problems/subsets/solution.py
@@ -2,17 +2,11 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
         for i in range(0, (1<<len(nums))):
-            self.bitManp(nums, ans, i)
+            ds = []
+            for j in range(0, len(nums)):
+                if i&(1<<j):
+                    ds.append(nums[j])
+            ans.append(ds)
         
         return ans
-    def bitManp(self, nums, ans, i):
-        j=0
-        v = []
-        while i>0:
-            if(i&1==1):
-                v.append(nums[j])
-            j+=1
-            i=i>>1
-        
-        ans.append(v)
-        return
+    
