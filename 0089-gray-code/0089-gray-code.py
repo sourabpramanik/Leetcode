6d7b7aaeb507d4bcc1deeb23e665f2c22d2f9e00
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-10-31 09:17:12 +0530

    Time: 104 ms (96.01%), Space: 21.5 MB (45.88%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,7 +1,11 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
         ans = [0]
-        for i in range(0,n):            
-            ans += [x+(1<<i) for x in reversed(ans)]
+        # O(n2)        
+        # for i in range(0,n):            
+        #     ans += [x+(1<<i) for x in reversed(ans)]
         
+        # O(n)
+        for i in range(1, 1<<n):
+            ans.append(ans[-1] ^ (i & -i))
         return ans
