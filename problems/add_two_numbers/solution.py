953dcae10577b6307a83712aa5ca8984621ab749
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-01 03:37:42 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- problems/add_two_numbers/solution.py
+++ problems/add_two_numbers/solution.py
@@ -5,49 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        ret = dum = ListNode(-1)
-        carry = 0
-        
         p1 = l1
         p2 = l2
-        
+        ans = ListNode(0)
+        dum = ans
+        carry = 0
         while p1 and p2:
             value = p1.val + p2.val + carry
             mod = value%10
             carry = value//10
-            
             dum.next = ListNode(mod)
-            dum = dum.next
-            
             p1 = p1.next
             p2 = p2.next
+            dum = dum.next
         
-        if(p1):
-            while(p1):
-                value = p1.val + carry
-                mod = value%10
-                carry = value//10
-                
-                dum.next = ListNode(mod)
-                dum = dum.next
-
-                p1 = p1.next
-            if(carry!=0):                
-                dum.next = ListNode(carry)
-        elif(p2):
-            while(p2):
-                value = p2.val + carry
-                mod = value%10
-                carry = value//10
-                
-                dum.next = ListNode(mod)
-                dum = dum.next
-
-                p2 = p2.next
-            if(carry!=0):
-                dum.next = ListNode(carry)
-        else:
-            if(carry!=0):
-                dum.next = ListNode(carry)
-        return ret.next
+        while p1:
+            value = p1.val + carry
+            mod = value%10
+            carry = value//10
+            dum.next = ListNode(mod)
+            p1 = p1.next
+            dum = dum.next
+        
+        while p2:
+            value = p2.val + carry
+            mod = value%10
+            carry = value//10
+            dum.next = ListNode(mod)
+            p2 = p2.next
+            dum = dum.next
+        if carry!=0:
+            dum.next = ListNode(carry)
+        return ans.next
+            
         
