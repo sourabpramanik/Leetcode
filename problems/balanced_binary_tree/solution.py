9f9813ef63f9473aea53e1e00b015ed7bc6a9ce2
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-09 03:29:46 +0000

    Sync LeetCode submission - Balanced Binary Tree (python3)

--- problems/balanced_binary_tree/solution.py
+++ problems/balanced_binary_tree/solution.py
@@ -6,19 +6,20 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if root is None: return 0
-        
-            lh = dfs(root.left)
-            if lh==-1: return -1
-            rh = dfs(root.right)
-            if rh==-1: return -1
-
-
-            if(abs(lh-rh)>1):
+        def dfs(node):
+            if not node:
+                return 0
+            
+            lh=dfs(node.left)
+            if lh==-1:
                 return -1
-
+            rh=dfs(node.right)
+            if rh==-1:
+                return -1
+            
+            if abs(lh-rh)>1:
+                return -1
+            
             return max(lh, rh)+1
+        
         return dfs(root)!=-1
-        
-        
