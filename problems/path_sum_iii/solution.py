6acf23d7bb8753b4cfd834ecc4338776314ea525
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-10 04:17:32 +0000

    Sync LeetCode submission - Path Sum III (python3)

--- problems/path_sum_iii/solution.py
+++ problems/path_sum_iii/solution.py
@@ -5,16 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rec(self, node, T):
-        if node:
-            return int(T==node.val) + self.rec(node.left, T-node.val) + self.rec(node.right, T-node.val)       
+    def pathSum(self, root: Optional[TreeNode], target: int) -> int:
+             
+        path = {0:1}
         
-        return 0
-    def pathSum(self, root: Optional[TreeNode], T: int) -> int:
-        if root:
-            return self.rec(root, T) + self.pathSum(root.left, T) + self.pathSum(root.right, T)         
-        return 0
-    
-    
+        def rec(node, pre_sum):
+            if not node:
+                return 0
+            pre_sum+=node.val
+            count = path.get(pre_sum-target, 0)
+            path[pre_sum] = path.get(pre_sum, 0) + 1
+            count += rec(node.left, pre_sum) + rec(node.right, pre_sum)
+            path[pre_sum]-=1
+
+            return count
         
-        
+        return rec(root, 0)
+                
