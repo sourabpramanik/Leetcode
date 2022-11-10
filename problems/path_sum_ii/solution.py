c40f58efe42dded0cd5464d430dbb153218bb871
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-10 02:14:06 +0000

    Sync LeetCode submission - Path Sum II (python3)

--- problems/path_sum_ii/solution.py
+++ problems/path_sum_ii/solution.py
@@ -5,12 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def pathSum(self, root: Optional[TreeNode], T: int) -> List[List[int]]:
-        if root is None: return []
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         
-        if root.left is None and root.right is None and T-root.val==0:
-            return [[root.val]]
+        if not root:
+            return []
+        res = []
+        stack = [(root, targetSum-root.val, [root.val])]
         
-        temp = self.pathSum(root.left, T-root.val) + self.pathSum(root.right, T-root.val)
-        
-        return [[root.val]+i for i in temp]
+        while stack:
+            node, target, ds = stack.pop()
+            if not node.left and not node.right and target==0:
+                res.append(ds)
+            
+            if node.left:
+                stack.append((node.left, target-node.left.val, ds+[node.left.val]))
+            if node.right:
+                stack.append((node.right, target-node.right.val, ds+[node.right.val]))
+            
+        return res
