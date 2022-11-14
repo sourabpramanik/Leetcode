b57df942795ef902a53f7201e5529ec234c6db2d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-14 02:05:03 +0000

    Sync LeetCode submission - Rotate Image (python3)

--- problems/rotate_image/solution.py
+++ problems/rotate_image/solution.py
@@ -3,23 +3,11 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = len(matrix)
-        cols = len(matrix[0])
-        l=0
-        r=rows-1
-        
-        while l<r:
-            matrix[l], matrix[r]=matrix[r], matrix[l]
-            l+=1
-            r-=1
-        
-        for i in range(rows):            
-            for j in range(i+1, cols):
-                temp = matrix[i][j]
-                matrix[i][j]=matrix[j][i]
-                matrix[j][i]=temp
-        
+        n = len(matrix)
        
+        for i in range(0, n):
+            for j in range(0, i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        
-        
+        for i in range(0, n):
+            matrix[i] = matrix[i][::-1]
