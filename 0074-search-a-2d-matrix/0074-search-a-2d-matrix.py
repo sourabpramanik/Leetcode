f1c56a939e798c6f1c342cfa27a02562d80248e8
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-15 19:56:17 +0530

    Time: 47 ms (92.29%), Space: 14.4 MB (89.76%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,17 +3,17 @@ class Solution:
         m=len(matrix)
         n=len(matrix[0])
         
-        for i in range(0, m):
-            if matrix[i][-1]>=target:
-                l=0
-                r=n
-                
-                while(l<r):
-                    mid = (l+r)//2
-                    if matrix[i][mid]==target:
-                        return True
-                    elif matrix[i][mid]>target:
-                        r-=1
-                    else:
-                        l+=1
+        l=0
+        r=(n*m)-1
+        
+        while l<=r:
+            mid=(l+r)//2
+            
+            val = matrix[mid//n][mid%n]
+            if val==target:
+                return True
+            elif val>target:
+                r=mid-1
+            else:
+                l=mid+1
         return False
