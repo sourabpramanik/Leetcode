7ae3df065f25759a17484e9ebe4ae6dc33d5d9b1
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-16 14:18:38 +0000

    Sync LeetCode submission - Guess Number Higher or Lower (python3)

--- problems/guess_number_higher_or_lower/solution.py
+++ problems/guess_number_higher_or_lower/solution.py
@@ -7,15 +7,14 @@
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        low = 1
-        high = n
-        while low<=high:
-            mid = (high+low)//2
-            res = guess(mid)
-            if res==0:
+        l=1
+        h=n
+        while l<=h:
+            mid=(l+h)//2
+            
+            if guess(mid)==0:
                 return mid
-            elif(res<0):
-                high=mid-1
+            elif guess(mid)==-1:
+                h=mid-1
             else:
-                low=mid+1
-        return -1
+                l=mid+1
