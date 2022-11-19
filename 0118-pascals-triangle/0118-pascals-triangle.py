89210a3ba32e1bb08c4f819750eb41c0193f3543
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-19 12:35:46 +0530

    Time: 65 ms (19.68%), Space: 13.9 MB (18.85%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,12 +1,11 @@
 class Solution:
-    def generate(self, n: int) -> List[List[int]]:
-        v = [0]*n
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = [0]*numRows
         
-        for i in range(0, n):
-            v[i] = [0]*(i+1)
-            v[i][0]=1
-            v[i][-1]=1
+        for i in range(0, numRows):
+            ans[i] = [0]*(i+1)
+            ans[i][0] = 1
+            ans[i][-1] = 1
             for j in range(1, i):
-                v[i][j] = v[i-1][j-1] + v[i-1][j]
-        
-        return v
+                ans[i][j] = ans[i-1][j-1]+ans[i-1][j]
+        return ans
