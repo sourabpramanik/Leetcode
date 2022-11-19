51606b48d7aeb15c74beec974ecb8a7a5b29e793
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-19 10:54:46 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- problems/merge_sorted_array/solution.py
+++ problems/merge_sorted_array/solution.py
@@ -2,22 +2,40 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """
-        i=m-1 
-        j=n-1
-        k=m+n-1
+        """        
         
-        while(i>=0 and j>=0):
-            if(nums1[i]<nums2[j]):
-                nums1[k]=nums2[j]            
-                j-=1
-            else:
-                nums1[k]=nums1[i]
-                i-=1
-            k-=1
+        if m!=0:
+            gap = math.ceil((m+n)/2)
+            p1=0
+
+            while gap!=0:
+                p1=0
+                p2=gap
+
+                while p2<(n+m):
+                    
+                    if p2<m and nums1[p1]>nums1[p2]:
+                        nums1[p1], nums1[p2] = nums1[p2], nums1[p1]
+                        
+                    elif p2>=m and p1<m and nums1[p1]>nums2[p2-m]:
+                        nums1[p1],nums2[p2-m] = nums2[p2-m],nums1[p1]
+                        
+                    elif p2>=m and p1>=m and nums2[p1-m]>nums2[p2-m]:
+                        nums2[p1-m],nums2[p2-m] = nums2[p2-m],nums2[p1-m]
+                        
+
+                    p1+=1
+                    p2+=1
+
+                if gap==1:
+                    gap=0
+                else:
+                    gap = math.ceil(gap/2)
+                    
+        for num in nums2:
+            nums1[m]=num
+            m+=1
+            
         
-        while(j>=0):
-            nums1[k]=nums2[j]            
-            j-=1
-            k-=1    
+        
         
