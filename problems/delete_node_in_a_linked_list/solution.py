d02357c6acdfbbccaef2eab256086f3034d1112a
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-20 15:48:45 +0000

    Sync LeetCode submission - Delete Node in a Linked List (python3)

--- problems/delete_node_in_a_linked_list/solution.py
+++ problems/delete_node_in_a_linked_list/solution.py
@@ -10,8 +10,6 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        temp = node.next
-        node.val = temp.val
-        node.next = temp.next
-        temp.next = None
+        node.val= node.next.val
+        node.next = node.next.next
         
