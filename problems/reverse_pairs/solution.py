847cf4807f03433e2bce6683488b6529e47c3121
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-20 04:02:41 +0000

    Sync LeetCode submission - Reverse Pairs (python3)

--- problems/reverse_pairs/solution.py
+++ problems/reverse_pairs/solution.py
@@ -1,52 +1,48 @@
 class Solution:
-    def __init__(self):
-        self.count=0
-        
     def reversePairs(self, nums: List[int]) -> int:
-        
-                
-        return self.mergeSort(nums,0, len(nums)-1)
-    def mergeSort(self,nums, l, r):                
-        if l>=r: return 0
-        mid = (l+r)//2
-        self.count= self.mergeSort(nums, l, mid)
-        self.count+= self.mergeSort(nums, mid+1, r)
-        self.count+= self.merge(l,mid,r,nums)
+        self.count=0
+        self.mergeSort(nums, 0, len(nums)-1)
         return self.count
     
-    def merge(self, l, mid, r, nums):
+    def mergeSort(self, nums, l, r):
+        if l==r:
+            return 0
+        mid = (l+r)//2
+        self.count=self.mergeSort(nums, l, mid)        
+        self.count+=self.mergeSort(nums, mid+1, r)
+        self.count+=self.merge(nums, l, mid, r)
         
-        j=mid+1
-        
+        return self.count
+    
+    def merge(self, nums, left, mid, right):
+        ds=[]
         count=0
-        for i in range(l, mid+1):
-            while j<=r and nums[i]>2*nums[j]:
+        j=mid+1
+        for i in range(left, mid+1):            
+            while j<=right and nums[i]>2*nums[j]:                
                 j+=1
             count+=(j-(mid+1))
-            
-        left=l
-        right=mid+1
-        ds =[]
         
-        while left<=mid and right<=r:
-            if nums[left]<=nums[right]:
-                ds.append(nums[left])
-                left+=1
+        i=left
+        j=mid+1
+    
+        while i<=mid and j<=right:
+            if nums[i]<=nums[j]:
+                ds.append(nums[i])
+                i+=1
             else:
-                ds.append(nums[right])
-                right+=1
-        
-        while left<=mid:
-            ds.append(nums[left])
-            left+=1
-        
-        while right<=r:
-            ds.append(nums[right])
-            right+=1
-            
-        for i in range(l, r+1):
-            nums[i] = ds[i-l]
-        
-        return count
+                ds.append(nums[j])
+                j+=1
            
-                
+            
+        while i<=mid:
+            ds.append(nums[i])
+            i+=1
+        
+        while j<=right:
+            ds.append(nums[j])
+            j+=1
+        
+        for i in range(left, right+1):
+            nums[i] = ds[i-left]
+        return count
