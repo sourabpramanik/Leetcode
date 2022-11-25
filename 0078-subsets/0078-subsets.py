cf55e170404be77ee814f5f5889f7cd98fa8aec5
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-25 08:41:13 +0530

    Time: 51 ms (74.65%), Space: 14.1 MB (82.81%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        for i in range(0, (1<<len(nums))):
-            ds = []
-            for j in range(0, len(nums)):
-                if i&(1<<j):
-                    ds.append(nums[j])
-            ans.append(ds)
+        ans = [[]]
+        for num in nums:
+            n=len(ans)
+           
+            for i in range(0, n):
+                tmp=ans[i]+[num]
+                ans+=[tmp]
+                
+            
+            
         
         return ans
+                
     
