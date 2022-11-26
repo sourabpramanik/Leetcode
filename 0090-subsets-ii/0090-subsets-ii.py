c84f06764ff2d61edc22e1946e200119a09cc9a4
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-26 20:39:40 +0530

    Time: 70 ms (58.16%), Space: 14.1 MB (49.81%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,26 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        seen = set()
+        ans=[]
+        n=len(nums)
         nums.sort()
-        for i in range(0, (1<<len(nums))):
-            self.bitMap(nums, seen, ans, i)
-        
-        return ans
-    
-    def bitMap(self, nums, seen, ans, i):
-        j=0
-        v=[]
-        
-        while i>0:
+        def rec(i,ds):
+            ans.append(ds[:])
+            if i>=n:
+                return
             
-            if i&1:
-                v.append(nums[j])
-            j+=1
-            i = i>>1
-        tup = tuple(v)
-        if tup not in seen:
-            seen.add(tup)
-            ans.append(v)
-        return
-        
+            for j in range(i,n):
+                if j>i and nums[j]==nums[j-1]:
+                    continue
+                ds.append(nums[j])
+                rec(j+1, ds)
+                ds.pop(len(ds)-1)
+        rec(0, [])
+        return ans
