873767a6a882b25c24722bd4e304424c52b05728
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 08:35:00 +0530

    Time: 71 ms (26.81%), Space: 14 MB (15.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -10,7 +10,7 @@ class Solution:
         i=0
         while i<len(nums):
             if 0 in hashMap and hashMap[0]>0:
-                nums[i]=0
+                nums[i]=0               
                 hashMap[0]-=1
             elif 1 in hashMap and hashMap[1]>0:
                 nums[i]=1
@@ -18,8 +18,11 @@ class Solution:
             elif 2 in hashMap and hashMap[2]>0:
                 nums[i]=2
                 hashMap[2]-=1
-        
-            i+=1
             
+            i+=1
+        
         
                 
+                
+        
+        
