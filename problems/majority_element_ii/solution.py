c97ab96d441805d6e1f5ab6e22c657881e4fb351
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 06:46:26 +0000

    Sync LeetCode submission - Majority Element II (python3)

--- problems/majority_element_ii/solution.py
+++ problems/majority_element_ii/solution.py
@@ -1,41 +1,39 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        element1 = 0        
-        element2 = 0        
-        count1 = 0        
-        count2 = 0
-        ans=[]
+        ele1=0        
+        ele2=0
+        c1=0
+        c2=0
+        
         for num in nums:
-            if element1==num:
-                count1+=1
-                
-            elif element2==num:                
-                count2+=1
-                
-            elif count1==0:
-                element1=num
-                count1+=1
-                
-            elif count2==0:
-                element2=num
-                count2+=1
-            
+            if ele1==num:
+                c1+=1
+            elif ele2==num:
+                c2+=1
+            elif c1==0:
+                ele1=num
+                c1+=1
+            elif c2==0:
+                ele2=num
+                c2+=1
             else:
-                count1-=1                
-                count2-=1
+                c1-=1
+                c2-=1
         
-        count1 = 0        
-        count2 = 0
+        c1=0
+        c2=0
+        
         for num in nums:
-            if element1==num:
-                count1+=1
-            elif element2==num:
-                count2+=1
+            if num==ele1:
+                c1+=1
+            elif num==ele2:
+                c2+=1
         
-        if count1>len(nums)/3:
-            ans.append(element1)
-        
-        if count2>len(nums)/3:
-            ans.append(element2)
+        ans=[]
+        if c1>len(nums)/3:
+            ans.append(ele1)
+            
+        if c2>len(nums)/3:
+            ans.append(ele2)
         
         return ans
