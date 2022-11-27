74d3939879acb5f5fb9749e4eac2f21619d1a80c
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 02:31:20 +0000

    Sync LeetCode submission - Pascal's Triangle (python3)

--- problems/pascal's_triangle/solution.py
+++ problems/pascal's_triangle/solution.py
@@ -1,11 +1,12 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        ans = [0]*numRows
-        
-        for i in range(0, numRows):
+    def generate(self, n: int) -> List[List[int]]:
+        ans=[0]*n
+        ans[0]=[1]
+        for i in range(1, n):
             ans[i] = [0]*(i+1)
-            ans[i][0] = 1
-            ans[i][-1] = 1
+            ans[i][0]=1            
+            ans[i][-1]=1
             for j in range(1, i):
-                ans[i][j] = ans[i-1][j-1]+ans[i-1][j]
+                ans[i][j]=ans[i-1][j-1] + ans[i-1][j]
+        
         return ans
