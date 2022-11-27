34058a99cc29b5bcf29f28d51ea2d9e5c44049b9
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 04:12:09 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- problems/merge_sorted_array/solution.py
+++ problems/merge_sorted_array/solution.py
@@ -2,40 +2,34 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-        """        
-        
+        """
         if m!=0:
-            gap = math.ceil((m+n)/2)
-            p1=0
-
+            gap=ceil((m+n)/2)
             while gap!=0:
-                p1=0
-                p2=gap
+                p=0
+                q=gap
+                
+                while q<(m+n):
+                    if q<m and nums1[p]>nums1[q]:
+                        nums1[p], nums1[q] = nums1[q], nums1[p]
+                
+                    elif p<m and q>=m and nums1[p]>nums2[q-m]:
+                        nums1[p], nums2[q-m] = nums2[q-m], nums1[p]
 
-                while p2<(n+m):
-                    
-                    if p2<m and nums1[p1]>nums1[p2]:
-                        nums1[p1], nums1[p2] = nums1[p2], nums1[p1]
-                        
-                    elif p2>=m and p1<m and nums1[p1]>nums2[p2-m]:
-                        nums1[p1],nums2[p2-m] = nums2[p2-m],nums1[p1]
-                        
-                    elif p2>=m and p1>=m and nums2[p1-m]>nums2[p2-m]:
-                        nums2[p1-m],nums2[p2-m] = nums2[p2-m],nums2[p1-m]
-                        
+                    elif p>=m and q>=m and nums2[p-m]>nums2[q-m]:
+                        nums2[p-m], nums2[q-m] = nums2[q-m], nums2[p-m]
 
-                    p1+=1
-                    p2+=1
-
+                    p+=1
+                    q+=1
+                
                 if gap==1:
                     gap=0
                 else:
-                    gap = math.ceil(gap/2)
-                    
+                    gap = ceil(gap/2)
+                
+               
+          
         for num in nums2:
-            nums1[m]=num
+            nums1[m] = num
             m+=1
             
-        
-        
-        
