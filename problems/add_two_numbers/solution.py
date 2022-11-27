a5ec1b2a89ad2970f63533f0b49bb1f72abe679c
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 11:31:41 +0000

    Sync LeetCode submission - Add Two Numbers (python3)

--- problems/add_two_numbers/solution.py
+++ problems/add_two_numbers/solution.py
@@ -5,37 +5,35 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        p1 = l1
-        p2 = l2
-        ans = ListNode(0)
-        dum = ans
-        carry = 0
-        while p1 and p2:
-            value = p1.val + p2.val + carry
-            mod = value%10
-            carry = value//10
-            dum.next = ListNode(mod)
-            p1 = p1.next
-            p2 = p2.next
-            dum = dum.next
+        dummy=ListNode(0)
+        p=dummy
+        carry=0
+        while l1 and l2:
+            nodeSum = l1.val + l2.val + carry
+            carry = nodeSum//10
+            node = ListNode(nodeSum%10)
+            p.next = node
+            p=p.next
+            l1=l1.next
+            l2=l2.next
         
-        while p1:
-            value = p1.val + carry
-            mod = value%10
-            carry = value//10
-            dum.next = ListNode(mod)
-            p1 = p1.next
-            dum = dum.next
+        while l1:
+            nodeSum = l1.val + carry
+            carry = nodeSum//10
+            node = ListNode(nodeSum%10)
+            p.next = node
+            p=p.next
+            l1=l1.next
         
-        while p2:
-            value = p2.val + carry
-            mod = value%10
-            carry = value//10
-            dum.next = ListNode(mod)
-            p2 = p2.next
-            dum = dum.next
-        if carry!=0:
-            dum.next = ListNode(carry)
-        return ans.next
-            
+        while l2:
+            nodeSum = l2.val + carry
+            carry = nodeSum//10
+            node = ListNode(nodeSum%10)
+            p.next = node
+            p=p.next
+            l2=l2.next
         
+        if carry:
+            p.next = ListNode(carry)
+        
+        return dummy.next
