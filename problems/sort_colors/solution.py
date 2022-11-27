d0ee0118e77eeeeb7cfc1ba4e857a9aa37719deb
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 03:07:48 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- problems/sort_colors/solution.py
+++ problems/sort_colors/solution.py
@@ -3,23 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        hashMap = {}
-        for num in nums:
-            hashMap[num] = hashMap.get(num, 0)+1
-        
-        i=0
-        while i<len(nums):
-            if 0 in hashMap and hashMap[0]>0:
-                nums[i]=0
-                hashMap[0]-=1
-            elif 1 in hashMap and hashMap[1]>0:
-                nums[i]=1
-                hashMap[1]-=1
-            elif 2 in hashMap and hashMap[2]>0:
-                nums[i]=2
-                hashMap[2]-=1
-        
-            i+=1
+        low, mid, high = 0, 0, len(nums)-1
+        while mid<=high:
             
+            if nums[mid]==0:
+                nums[low], nums[mid]=nums[mid], nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:            
+                mid+=1
+            elif nums[mid]==2:
+                nums[high], nums[mid]=nums[mid], nums[high]
+                high-=1
+        
         
                 
+                
+        
+        
