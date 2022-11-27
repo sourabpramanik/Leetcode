6d535e117aadb1bce1628842e3f7c8a68fda68ee
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 02:53:55 +0000

    Sync LeetCode submission - Next Permutation (python3)

--- problems/next_permutation/solution.py
+++ problems/next_permutation/solution.py
@@ -3,29 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = len(nums)-1
+        n=len(nums)
+        i=n-1
+        
         while i>=0:
-            if i<len(nums)-1 and nums[i]<nums[i+1]:
+            if i<n-1 and nums[i]<nums[i+1]:
                 break
             i-=1
-        
-        j = len(nums)-1
-        
+            
         if i>=0:
+            j=n-1
             while j>=0:
-                if nums[i]<nums[j]:
+                if nums[j]>nums[i]:
                     break
                 j-=1
-
+            
             nums[i], nums[j] = nums[j], nums[i]
         
         i=i+1
-        j=len(nums)-1
+        j=n-1
         
         while i<=j:
             nums[i], nums[j] = nums[j], nums[i]
             i+=1
             j-=1
-            
-        
             
