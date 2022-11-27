a912bdfe0ded011b70710ff551355358978923b5
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 08:49:00 +0530

    Time: 91 ms (5.28%), Space: 13.8 MB (75.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -4,15 +4,16 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        
         for i in range(0, n):
-            for j in range(0, i):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j]=matrix[i][j], matrix[j][i]
+        
         
         for i in range(0, n):
             l=0
-            r=len(matrix)-1
-            while l<=r:
+            r=n-1
+            while(l<=r):
                 matrix[i][l], matrix[i][r] = matrix[i][r], matrix[i][l]
                 l+=1
                 r-=1
+                
