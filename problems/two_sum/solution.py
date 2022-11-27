221032458c86655f199073787e9ff5e4a19316bb
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 09:41:51 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,16 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        hashmap = dict()
-        
-        i = 1
-        for v in nums:
-            diff = target - v
-            
-            if not diff in hashmap:
-                hashmap[v] = i
-                i+=1
+        s=0
+        e=len(nums)-1
+        n1=0
+        n2=0
+        tmp = []
+        for i in range(0, len(nums)):
+            tmp.append(nums[i])
+        tmp.sort()
+        while s<e:
+            if tmp[s]+tmp[e]==target:
+                n1=tmp[s]
+                n2=tmp[e]
+                break
+            elif tmp[s]+tmp[e]>target:
+                e-=1
             else:
-                return [hashmap[diff]-1, i-1]
+                s+=1
                 
-     
+        res=[]
+        
+        for i in range(0, len(nums)):
+            if nums[i]==n1:
+                res.append(i)
+            elif nums[i]==n2:
+                res.append(i)
+        return res
