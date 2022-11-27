e4e8f5d1a983a7bc08fa717807d047a84f006c66
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-27 14:43:08 +0530

    Time: 4249 ms (43.07%), Space: 21.5 MB (70.11%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,48 +1,47 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        self.count=0
-        self.mergeSort(nums, 0, len(nums)-1)
-        return self.count
+        count=0
+        return self.divide(nums, 0, len(nums)-1)
     
-    def mergeSort(self, nums, l, r):
-        if l==r:
-            return 0
-        mid = (l+r)//2
-        self.count=self.mergeSort(nums, l, mid)        
-        self.count+=self.mergeSort(nums, mid+1, r)
-        self.count+=self.merge(nums, l, mid, r)
+    def divide(self, nums, l, r):
+        count=0
+        if l<r:
+            mid=(l+r)//2
+            count+= self.divide(nums, l, mid)            
+            count+= self.divide(nums, mid+1, r)
+            count+= self.merge(nums, l, mid, r)
         
-        return self.count
+        return count
     
-    def merge(self, nums, left, mid, right):
+    def merge(self, nums, l, mid, r):
         ds=[]
         count=0
-        j=mid+1
-        for i in range(left, mid+1):            
-            while j<=right and nums[i]>2*nums[j]:                
+        j=mid+1        
+        for i in range(l, mid+1):
+            while j<=r and nums[i]>2*nums[j]:
                 j+=1
             count+=(j-(mid+1))
-        
-        i=left
+            
+        i=l
         j=mid+1
-    
-        while i<=mid and j<=right:
+        
+        while i<=mid and j<=r:
             if nums[i]<=nums[j]:
                 ds.append(nums[i])
                 i+=1
             else:
                 ds.append(nums[j])
                 j+=1
-           
-            
+        
         while i<=mid:
             ds.append(nums[i])
             i+=1
         
-        while j<=right:
+        while j<=r:
             ds.append(nums[j])
             j+=1
+            
+        for i in range(l, r+1):
+            nums[i]=ds[i-l]
         
-        for i in range(left, right+1):
-            nums[i] = ds[i-left]
         return count
