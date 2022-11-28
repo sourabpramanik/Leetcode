390fe0973eab2a9d35766e20faf7cb3afb399089
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-28 19:56:54 +0530

    Time: 2049 ms (9.69%), Space: 13.8 MB (96.50%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -3,32 +3,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """   
+        self.grid = grid
+        self.n = len(grid)
+        self.solve()
         
-        self.solve(grid, len(grid))
-        
-    def solve(self, grid, n):
-        for i in range(0, n):
-            for j in range(0, n):
-                if grid[i][j]==".":
+    def solve(self):
+        for i in range(0, self.n):
+            for j in range(0, self.n):
+                if self.grid[i][j]==".":
                     for num in ["1","2","3","4","5","6","7","8","9"]:
-                        if self.valid(grid, i, j, num):
-                            grid[i][j] = num
-                            if self.solve(grid, n):
+                        if self.valid(i, j, num):
+                            self.grid[i][j] = num
+                            if self.solve():
                                 return True
                             else:
-                                grid[i][j] = "."
+                                self.grid[i][j] = "."
                     return False
         return True
                 
     
-    def valid(self, grid, row, col, char):
+    def valid(self, row, col, char):
         for i in range(0, 9):
-            if grid[i][col]==char:
+            if self.grid[i][col]==char:
                 return False
             
-            if grid[row][i]==char:
+            if self.grid[row][i]==char:
                 return False
-            if grid[3* (row//3) + (i//3)][3* (col//3) + (i%3)]==char:
+            if self.grid[3* (row//3) + (i//3)][3* (col//3) + (i%3)]==char:
                 return False
         
         return True
