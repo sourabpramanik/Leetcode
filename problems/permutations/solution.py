9dc96d748af398b1544db1b059c24f96cc8ee736
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-11-28 02:34:53 +0000

    Sync LeetCode submission - Permutations (python3)

--- problems/permutations/solution.py
+++ problems/permutations/solution.py
@@ -1,25 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans=[]
-        
-        freq = {}
-        for i in range(len(nums)):
-            freq[i] = 0
-        
-        def rec(ds):
-            if len(ds)==len(nums):
+        n=len(nums)
+        def rec(i, ds):
+            if i>=n:
                 ans.append(ds[:])
                 return
             
-            for j in range(0, len(nums)):
-                if freq[j]==0:
-                    
-                    freq[j] = 1
-                    ds.append(nums[j])   
-                    
-                    rec(ds)
-                    
-                    ds.pop(len(ds)-1)
-                    freq[j]=0                
-        rec([])
+            for j in range(i, n):
+                ds[i], ds[j] = ds[j], ds[i]
+                rec(i+1, ds)
+                ds[i], ds[j] = ds[j], ds[i]
+                
+        rec(0, nums)
         return ans
