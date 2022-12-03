a2f6290b9d8a2a15e409987e6cee57a42332e260
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-03 17:04:23 +0000

    Sync LeetCode submission - Next Permutation (python3)

--- problems/next_permutation/solution.py
+++ problems/next_permutation/solution.py
@@ -5,26 +5,26 @@ class Solution:
         """
         n=len(nums)
         i=n-1
-        
         while i>=0:
             if i<n-1 and nums[i]<nums[i+1]:
                 break
             i-=1
-            
+                        
+        
         if i>=0:
             j=n-1
-            while j>=0:
-                if nums[j]>nums[i]:
-                    break
+            while j>=0 and nums[j]<=nums[i]:
                 j-=1
-            
+                
             nums[i], nums[j] = nums[j], nums[i]
         
-        i=i+1
+        i+=1
         j=n-1
         
-        while i<=j:
+        while i<j:
             nums[i], nums[j] = nums[j], nums[i]
             i+=1
             j-=1
             
+        
+        
