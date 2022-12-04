0abd54ceda4b2110e7f5e1d4f04abb2fb191244d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-04 08:45:47 +0000

    Sync LeetCode submission - 4Sum (python3)

--- problems/4sum/solution.py
+++ problems/4sum/solution.py
@@ -1,31 +1,32 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n=len(nums)
         ans=[]
         nums.sort()
-        for i in range(0, n):
-            if i>0 and nums[i-1]==nums[i]:
+        n=len(nums)
+        for i in range(0, n-2):
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            for j in range(i+1, n):
-                if j>i+1 and nums[j-1]==nums[j]:
+            for j in range(i+1, n-1):
+                if j>i+1 and nums[j]==nums[j-1]:
                     continue
                 l=j+1
-                r=len(nums)-1
-                diff=target-nums[i]-nums[j]
+                r=n-1
+                dif=target-nums[i]-nums[j]
+                
                 while l<r:
                     val=nums[l]+nums[r]
-                    if diff==val:
-                        ans.append([nums[i], nums[j], nums[l], nums[r]])
+                    if val==dif:
+                        ans.append([nums[i],nums[j],nums[l],nums[r]])
                         while l<r and nums[l]==nums[l+1]:
                             l+=1
                         while l<r and nums[r]==nums[r-1]:
                             r-=1
+                        
                         l+=1
                         r-=1
-                    elif diff<val:
+                    elif val>dif:
                         r-=1
                     else:
                         l+=1
-                        
         
         return ans
