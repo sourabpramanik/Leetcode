1d4b370197c07e781837603f1a0e4c2b7014e30e
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-04 07:02:52 +0000

    Sync LeetCode submission - Unique Paths (python3)

--- problems/unique_paths/solution.py
+++ problems/unique_paths/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         N=m+n-2
-        r=n-1
+        r=m-1
         ans=1
         
-        for i  in range(1, r+1):
-            ans*= N-r+i
+        for i in range(1, r+1):
+            ans *= N-r+i
             ans //= i
         
         return ans
