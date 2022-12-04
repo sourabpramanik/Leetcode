9e61519d55bcc1b3596432b3b023eb72c16ada5e
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-04 07:32:50 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,28 +1,35 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        s=0
-        e=len(nums)-1
+        
+        n=len(nums)
+        arr=[]
+        for i in range(0, n):
+            arr.append(nums[i])
+        arr.sort()
+        
+        l=0
+        r=n-1
+        
         n1=0
         n2=0
-        tmp = []
-        for i in range(0, len(nums)):
-            tmp.append(nums[i])
-        tmp.sort()
-        while s<e:
-            if tmp[s]+tmp[e]==target:
-                n1=tmp[s]
-                n2=tmp[e]
-                break
-            elif tmp[s]+tmp[e]>target:
-                e-=1
-            else:
-                s+=1
-                
-        res=[]
         
-        for i in range(0, len(nums)):
-            if nums[i]==n1:
-                res.append(i)
-            elif nums[i]==n2:
-                res.append(i)
-        return res
+        while l<r:
+            if arr[l]+arr[r]==target:
+                n1=arr[l]
+                n2=arr[r]
+                break
+            elif arr[l]+arr[r]>target:
+                r-=1
+            else:
+                l+=1
+                
+        ans=[]
+        for i in range(0, n):
+            if nums[i]==n1:                
+                ans.append(i)
+            elif nums[i]==n2:                
+                ans.append(i)
+        return ans
+        
+        
+        
