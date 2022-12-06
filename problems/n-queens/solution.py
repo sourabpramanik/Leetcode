64c6e4955c38917b118dfbfd2f71cc600dfc0393
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-06 03:40:03 +0000

    Sync LeetCode submission - N-Queens (python3)

--- problems/n-queens/solution.py
+++ problems/n-queens/solution.py
@@ -1,47 +1,29 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans=[]
-        grid=["."*n]*n        
-        
-        def rec(j, grid):
-            if j==n:
-               
-                ans.append(grid[:])
-                return 
+        board=["."*n]*n
+        left=[0]*n
+        upDia=[0]*2*n
+        lowDia=[0]*2*n
+
+        def rec(j):
+            if j == n:
+                ans.append(board[:])
+                return
             
-            for i in range(n):
-                if self.safe(i, j, grid):                   
-                    grid[i]=grid[i][:j]+"Q"+grid[i][j+1:]                    
-                    rec(j+1, grid)                    
-                    grid[i]=grid[i][:j]+"."+grid[i][j+1:]
-        rec(0, grid)
-        
-        return ans      
+            for i in range(0, n):
+                if left[i]==0 and upDia[i+j]==0 and lowDia[n-1+j-i]==0:
+                    left[i]=1 
+                    upDia[i+j]=1
+                    lowDia[n-1+j-i]=1                    
+                    board[i]=board[i][:j]+"Q"+board[i][j+1:]
+                    rec(j+1)
+                    left[i]=0
+                    upDia[i+j]=0
+                    lowDia[n-1+j-i]=0 
+                    board[i]=board[i][:j]+"."+board[i][j+1:]
+        rec(0)
+
+        return ans
     
-    def safe(self, row, col, grid):
-        i=row
-        j=col
-        
-        while i>=0 and j>=0:
-            if grid[i][j]=="Q":
-                return False
-            i-=1
-            j-=1
-        
-        
-        j=col
-        
-        while j>=0:
-            if grid[row][j]=="Q":
-                return False            
-            j-=1
-        
-        i=row
-        j=col
-        while i<len(grid) and j>=0:
-            if grid[i][j]=="Q":
-                return False
-            i+=1
-            j-=1
-        
-        return True
+    
