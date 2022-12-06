b1a91518687c60a080389936947fc5bd26469737
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-06 01:32:42 +0000

    Sync LeetCode submission - Combination Sum II (python3)

--- problems/combination_sum_ii/solution.py
+++ problems/combination_sum_ii/solution.py
@@ -1,25 +1,25 @@
 class Solution:
-    def combinationSum2(self, arr: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, A: List[int], target: int) -> List[List[int]]:
         ans=[]
-        n=len(arr)
-        arr.sort()
-        def find(i, ds, targetSum):
-            
-            if targetSum==0:
+        n=len(A)
+        A.sort()
+        def rec(i, ds, target):
+            if target==0:
                 ans.append(ds[:])
                 return
             
-           
-            
-            
+            if i>=n:
+                return
+
             for j in range(i, n):
-                if targetSum<arr[j]: 
+                if target<A[j]:
                     break
-                if j>i and arr[j]==arr[j-1]:
+                if j>i and A[j]==A[j-1]:
                     continue
-                ds.append(arr[j])
-                find(j+1, ds, targetSum-arr[j])
-                ds.pop(len(ds)-1)
+                ds.append(A[j])
+                rec(j+1, ds, target-A[j])
+                ds.pop()                
             
-        find(0, [], target)
+        
+        rec(0, [], target)
         return ans
