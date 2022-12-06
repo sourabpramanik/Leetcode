dfb4c69d744e65e313c0459d8c801433270000b3
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-06 01:21:08 +0000

    Sync LeetCode submission - Combination Sum (python3)

--- problems/combination_sum/solution.py
+++ problems/combination_sum/solution.py
@@ -1,20 +1,20 @@
 class Solution:
-    def combinationSum(self, arr: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, A: List[int], target: int) -> List[List[int]]:
+        n=len(A)
         ans=[]
-        
-        def find(i, ds, targetSum):
-            if i>=len(arr):
-                return
-            
+        def rec(id, ds, targetSum):
             if targetSum==0:
                 ans.append(ds[:])
                 return
+
+            if id>=n:
+                return                         
             
-            if targetSum>=arr[i]:
-                ds.append(arr[i])
-                find(i, ds, targetSum-arr[i])
-                ds.pop(len(ds)-1)
-            find(i+1, ds, targetSum)
-        
-        find(0, [], target)
-        return ans
+            if targetSum>=A[id]:
+                ds.append(A[id])
+                rec(id, ds, targetSum-A[id])
+                ds.pop()
+            rec(id+1, ds, targetSum)
+
+        rec(0, [], target)
+        return ans            
