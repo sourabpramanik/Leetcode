e49e0494828a6904844f083b825c2f8c69fd9c21
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-07 00:29:11 +0000

    Sync LeetCode submission - Binary Search (python3)

--- problems/binary_search/solution.py
+++ problems/binary_search/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        low, high = 0, len(nums)-1
-        
-        while low <= high:
-            mid = (low+high)//2
-            
-            if nums[mid] < target:
-                low = mid+1
-            elif nums[mid] > target:
-                high = mid-1
-            else:
+        l=0
+        h=len(nums)-1
+
+        while l<=h:
+            mid = (l+h)>>1
+            if nums[mid]==target:
                 return mid
+            elif nums[mid]>target:
+                h=mid-1
+            else:
+                l=mid+1
+        
         return -1
