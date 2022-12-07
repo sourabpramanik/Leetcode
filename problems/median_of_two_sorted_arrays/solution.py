02258bd9f484feda91fc5dc1aeab68f7c72af44f
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-07 01:46:20 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (python3)

--- problems/median_of_two_sorted_arrays/solution.py
+++ problems/median_of_two_sorted_arrays/solution.py
@@ -2,31 +2,36 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1)>len(nums2):
             return self.findMedianSortedArrays(nums2, nums1)
-        
-        n1=len(nums1)        
+
+        n1=len(nums1)
         n2=len(nums2)
-        
+
         low=0
         high=n1
         
         while low<=high:
-            cut1=(low+high)//2
+            mid = (low+high)//2
+            cut1=mid
             cut2=(n1+n2+1)//2-cut1
-            
-            l1=float("-inf") if cut1==0 else nums1[cut1-1]            
-            l2=float("-inf") if cut2==0 else nums2[cut2-1]
-            
-            r1=float("inf") if cut1==n1 else nums1[cut1]            
-            r2=float("inf") if cut2==n2 else nums2[cut2]
-            
-            if l1<=r2 and l2<=r1:
+
+            l1= float("-inf") if cut1==0 else nums1[cut1-1]
+            l2= float("-inf") if cut2==0 else nums2[cut2-1]
+
+            r1= float("inf") if cut1==n1 else nums1[cut1]
+            r2= float("inf") if cut2==n2 else nums2[cut2]
+
+            if l1 <= r2 and l2 <= r1:
                 if (n1+n2)%2==1:
                     return max(l1, l2)
+                    
                 else:
-                    return (max(l1, l2)+min(r1, r2))/2
+                    return (max(l1, l2) + min(r1, r2))/2
             elif l1>r2:
                 high=cut1-1
             else:
                 low=cut1+1
         
         return 0.0
+
+
+
