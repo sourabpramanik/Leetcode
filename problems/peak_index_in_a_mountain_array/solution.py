16006df73299013890675a0e77b65fd23f18d8eb
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-10 11:07:24 +0000

    Sync LeetCode submission - Peak Index in a Mountain Array (python3)

--- problems/peak_index_in_a_mountain_array/solution.py
+++ problems/peak_index_in_a_mountain_array/solution.py
@@ -1,15 +1,17 @@
 class Solution:
-    def peakIndexInMountainArray(self, A: List[int]) -> int:
-        
-        s=0
-        e = len(A)-1
-     
-        while s<e:
-            mid = (s+e)//2
-            
-            if A[mid]<A[mid+1]:
-                s=mid+1
+    def peakIndexInMountainArray(self, nums: List[int]) -> int:
+
+        n=len(nums)
+
+        low=0
+        high=n-1
+
+        while low<=high:
+            mid=(low+high)>>1
+
+            if nums[mid]<nums[mid+1]:
+                low=mid+1
             else:
-                e=mid
-            
-        return s
+                high=mid-1
+
+        return low 
