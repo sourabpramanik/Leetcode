b051e2f6001242eba59f2d308c3a79d5467ef68c
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-13 16:12:30 +0000

    Sync LeetCode submission - Arranging Coins (python3)

--- problems/arranging_coins/solution.py
+++ problems/arranging_coins/solution.py
@@ -1,4 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return int((math.sqrt(8 * n + 1)-1)/2)
+        l=1
+        h=n
+
+        while l<=h:
+            mid=(l+h)>>1
+            if n>=mid*(mid+1)/2:
+                l=mid+1
+            else:
+                h=mid-1
         
+        return l-1
