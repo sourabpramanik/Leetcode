0faea6c1e1b34fb7935eae13bf4423474a850d30
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-18 14:58:26 +0000

    Sync LeetCode submission - Merge Sorted Array (python3)

--- problems/merge_sorted_array/solution.py
+++ problems/merge_sorted_array/solution.py
@@ -3,33 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        
         if m!=0:
             gap=ceil((m+n)/2)
             while gap!=0:
                 p=0
                 q=gap
-                
-                while q<(m+n):
-                    if q<m and nums1[p]>nums1[q]:
+
+                while q<m+n:
+                    if q<m and nums1[p] > nums1[q]:
                         nums1[p], nums1[q] = nums1[q], nums1[p]
-                
-                    elif p<m and q>=m and nums1[p]>nums2[q-m]:
-                        nums1[p], nums2[q-m] = nums2[q-m], nums1[p]
-
-                    elif p>=m and q>=m and nums2[p-m]>nums2[q-m]:
+                    elif p<m and q>=m and nums1[p] > nums2[q-m]:
+                        nums1[p], nums2[q-m] = nums2[q-m], nums1[p]                    
+                    elif p>=m and q>=m and nums2[p-m] > nums2[q-m]:
                         nums2[p-m], nums2[q-m] = nums2[q-m], nums2[p-m]
-
                     p+=1
                     q+=1
-                
                 if gap==1:
                     gap=0
                 else:
                     gap = ceil(gap/2)
-                
-               
-          
+            
         for num in nums2:
-            nums1[m] = num
+            nums1[m]=num
             m+=1
+
             
