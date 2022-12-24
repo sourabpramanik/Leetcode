2912e1b35a938e33df37dc9cfa4b521a7e354a9e
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-24 13:51:31 +0000

    Sync LeetCode submission - Reverse Pairs (python3)

--- problems/reverse_pairs/solution.py
+++ problems/reverse_pairs/solution.py
@@ -1,52 +1,46 @@
 class Solution:
-    def reversePairs(self, nums: List[int]) -> int:
-        
-        
-        return self.divide(nums, 0, len(nums)-1)
-    
-    def divide(self,arr, l, r):
-        if l>=r: return 0
-        mid=(l+r)//2
-        count = self.divide(arr, l, mid)
-        count += self.divide(arr, mid+1, r)
-        count += self.merge(arr, l, mid, r)
+    def reversePairs(self, arr: List[int]) -> int:
+        return self.divide(arr, 0, len(arr)-1)
+
+    def divide(self, arr, l, r):
+        if l>=r:
+            return 0
+        mid=(l+r)>>1
+        count=self.divide(arr, l, mid)
+        count+=self.divide(arr, mid+1, r)
+        count+=self.merge(arr, l, mid, r)
         return count
-    
-    def merge(self, arr, low, mid, high):
+
+    def merge(self, arr, left, mid, right):
+        j=mid+1
         count=0
-        j=mid+1
-        ds=[]
-        for i in range(low, mid+1):
-            while j<=high and arr[i]>2*arr[j]:
+        temp=[]
+        for i in range(left, mid+1):
+            while j<=right and arr[i]>2*arr[j]:
                 j+=1
-            
-            count+=j-(mid+1)
+            count+=j-mid-1
         
-        i=low
+        i=left
         j=mid+1
-        
-        while i<=mid and j<=high:
+
+        while i<=mid and j<=right:
             if arr[i]<=arr[j]:
-                ds.append(arr[i])
+                temp.append(arr[i])
                 i+=1
             else:
-                ds.append(arr[j])
+                temp.append(arr[j])
                 j+=1
         
         while i<=mid:
-            ds.append(arr[i])
+            temp.append(arr[i])
             i+=1
         
-        while j<=high:
-            ds.append(arr[j])
+        while j<=right:
+            temp.append(arr[j])
             j+=1
         
-        for i in range(low, high+1):
-            arr[i] = ds[i-low]
+        for i in range(left, right+1):
+            arr[i]=temp[i-left]
         
         return count
-        
-        
-            
-        
-        
+
