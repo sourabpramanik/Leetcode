1a132ee27e6f6355ecfd4923b07b85df7861db18
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-25 10:31:01 +0000

    Sync LeetCode submission - Palindrome Partitioning (python3)

--- problems/palindrome_partitioning/solution.py
+++ problems/palindrome_partitioning/solution.py
@@ -1,24 +1,21 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans=[]
-        n=len(s)
-        def rec(i, ds):
-            if i>=n:
-                ans.append(ds[:])
-                return
-            
-            for j in range(i, n):
-                if self.pal(s, i, j):
-                    ds.append(s[i:j+1])
-                    rec(j+1, ds)
-                    ds.pop()
-        
-        rec(0, [])
+        self.rec(0, [], s, len(s), ans)
         return ans
     
-    def pal(self, s, i, j):
-        l=i
-        r=j
+    def rec(self, i, ds, s, N, ans):
+        if i>=N:
+            ans.append(ds[:])
+            return
+        
+        for j in range(i, N):
+            if self.valid(s, i, j):
+                ds.append(s[i:j+1])
+                self.rec(j+1, ds, s, N, ans)
+                ds.pop()
+    
+    def valid(self, s, l, r):
         while l<=r:
             if s[l]!=s[r]:
                 return False
