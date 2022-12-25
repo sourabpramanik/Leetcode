5e52ee9f786700ff1873d0c80d45c24b40378ae8
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-25 12:02:07 +0000

    Sync LeetCode submission - Sudoku Solver (python3)

--- problems/sudoku_solver/solution.py
+++ problems/sudoku_solver/solution.py
@@ -1,35 +1,33 @@
 class Solution:
-    def solveSudoku(self, grid: List[List[str]]) -> None:
+    def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
-        """   
-        self.grid = grid
-        self.n = len(grid)
-        self.solve()
+        """
+        self.rec(board, len(board))
+    
+    def rec(self, board, n):
         
-    def solve(self):
-        for i in range(0, self.n):
-            for j in range(0, self.n):
-                if self.grid[i][j]==".":
-                    for num in ["1","2","3","4","5","6","7","8","9"]:
-                        if self.valid(i, j, num):
-                            self.grid[i][j] = num
-                            if self.solve():
+        for i in range(0, n):
+            for j in range(0, n):
+                if board[i][j]==".":
+                    for ch in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                        if self.valid(board, i, j, ch):
+                            board[i][j]=ch
+                            if self.rec(board, n):
                                 return True
                             else:
-                                self.grid[i][j] = "."
+                                board[i][j]="."
                     return False
         return True
-                
-    
-    def valid(self, row, col, char):
+        
+    def valid(self, board, row, col, ch):
         for i in range(0, 9):
-            if self.grid[i][col]==char:
+            if board[row][i]==ch:
                 return False
             
-            if self.grid[row][i]==char:
+            if board[i][col]==ch:
                 return False
-            if self.grid[3* (row//3) + (i//3)][3* (col//3) + (i%3)]==char:
+            if board[3*(row//3)+i//3][3*(col//3)+i%3]==ch:
                 return False
-        
         return True
+
