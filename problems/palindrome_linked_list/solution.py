3fcd8ca1ae50960d8208a28da157d0ecde79e66f
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-25 05:24:20 +0000

    Sync LeetCode submission - Palindrome Linked List (python3)

--- problems/palindrome_linked_list/solution.py
+++ problems/palindrome_linked_list/solution.py
@@ -5,36 +5,41 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast=head
-        slow=head
         
-        while fast and fast.next:
+        mid=self.getMid(head)
+        slow=head
+        while mid:
             slow=slow.next
-            fast=fast.next.next
+            mid-=1
         
         slow=self.reverse(slow)
-        
         p=head
-        
+
         while slow:
-            if p.val!=slow.val:
+            if slow.val!=p.val:
                 return False
             slow=slow.next
             p=p.next
         
         return True
+
+    def getMid(self, head):
+        mid=0
+        fast=head
+        while fast and fast.next:
+            mid+=1
+            fast=fast.next.next
+        
+        return mid
     
-    def reverse(self, node):
-        
-        pre=None
-        cur=node
-        nex=None
-        
+    def reverse(self, head):
+        prev=None
+        cur=head
+
         while cur:
             nex=cur.next
-            cur.next=pre
-            pre=cur
+            cur.next=prev
+            prev=cur
             cur=nex
         
-        return pre
-        
+        return prev
