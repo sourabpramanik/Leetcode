e012b7691e6a385831f38dfce898d7e6ed5bdf5d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-26 02:20:21 +0000

    Sync LeetCode submission - Single Element in a Sorted Array (python3)

--- problems/single_element_in_a_sorted_array/solution.py
+++ problems/single_element_in_a_sorted_array/solution.py
@@ -2,13 +2,11 @@ class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         l=0
         h=len(nums)-2
-        ans=0
         while l<=h:
             mid=(l+h)>>1
-
             if nums[mid]==nums[mid^1]:
                 l=mid+1
-            else:                
+            else:
                 h=mid-1
-
-        return nums[l]               
+        
+        return nums[l]
