e4d75d2e5f79aaa1051fe6a9224298606c11b915
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-27 01:06:00 +0000

    Sync LeetCode submission - Construct Binary Tree from Inorder and Postorder Traversal (python3)

--- problems/construct_binary_tree_from_inorder_and_postorder_traversal/solution.py
+++ problems/construct_binary_tree_from_inorder_and_postorder_traversal/solution.py
@@ -6,18 +6,19 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        if len(postorder)==0 or len(inorder)==0:
+        nodes = dict()
+        for i in range(0, len(inorder)):
+            nodes[inorder[i]] = i
+        return self.build(nodes, postorder, 0, len(postorder)-1, inorder, 0, len(inorder)-1)
+    
+    def build(self, nodes, postorder, posStart, posEnd, inorder, iStart, iEnd):
+        if posStart>posEnd or iStart>iEnd:
             return None
         
-        if len(inorder)==1:
-            return TreeNode(postorder[0])
-        
-        val = postorder.pop(len(postorder)-1)
-        idx = inorder.index(val)
-        
-        root = TreeNode(val)
-    
-        root.left = self.buildTree(inorder[:idx], postorder[:idx])        
-        root.right = self.buildTree(inorder[idx+1:], postorder[idx:])
-        
+        val=postorder[posEnd]
+        x=nodes[val]
+        nEle=x-iStart
+        root=TreeNode(val)
+        root.left= self.build(nodes, postorder, posStart, posStart+nEle-1, inorder, iStart, x-1)
+        root.right= self.build(nodes, postorder, posStart+nEle, posEnd-1, inorder, x+1, iEnd)
         return root
