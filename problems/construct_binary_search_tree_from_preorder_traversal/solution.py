f33268f569ab37fccbcf2a81714553913e5c6840
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-28 04:17:35 +0000

    Sync LeetCode submission - Construct Binary Search Tree from Preorder Traversal (python3)

--- problems/construct_binary_search_tree_from_preorder_traversal/solution.py
+++ problems/construct_binary_search_tree_from_preorder_traversal/solution.py
@@ -5,14 +5,25 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    i=0
-    def bstFromPreorder(self, preorder: List[int], bound=float("inf")) -> Optional[TreeNode]:
-                    
-        if len(preorder) == self.i or preorder[self.i]>bound: return None
-        root = TreeNode(preorder[self.i])
-        self.i+=1
-        root.left = self.bstFromPreorder(preorder, root.val)            
-        root.right = self.bstFromPreorder(preorder, bound)
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        inorder=sorted(preorder)
 
+        hashes={}
 
-        return root       
+        for i in range(0, len(preorder)):
+            hashes[inorder[i]] = i
+
+        return self.build(hashes, preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1)
+
+    def build(self, hashes, preorder, preStart, preEnd, inorder, iStart, iEnd):
+        if preStart>preEnd or iStart>iEnd:
+            return None
+        val=preorder[preStart]
+        x=hashes[val]
+        numleft=x-iStart
+        root=TreeNode(val)
+        root.left = self.build(hashes, preorder, preStart+1, preStart+numleft, inorder, iStart, x-1)    
+        root.right = self.build(hashes, preorder, preStart+numleft+1, preEnd, inorder, x+1, iEnd)   
+        return root 
+
+        
