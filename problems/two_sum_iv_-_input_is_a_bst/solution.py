3fee273b5c28509ca412a3b1fde7887db3806c4d
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2022-12-30 04:41:09 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (python3)

--- problems/two_sum_iv_-_input_is_a_bst/solution.py
+++ problems/two_sum_iv_-_input_is_a_bst/solution.py
@@ -4,31 +4,46 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-        
+
+class Iterator:
+    def __init__(self, root, flag):
+        self.stack=[]
+        self.left=flag
+        self.pushNode(root)
+    
+    def next(self):
+        cur=self.stack.pop()
+
+        if self.left:
+            self.pushNode(cur.right)
+        else:
+            self.pushNode(cur.left)
+
+        return cur.val 
+
+    def pushNode(self, node):
+        while node:
+            self.stack.append(node)
+            if self.left:
+                node=node.left
+            else:
+                node=node.right
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        self.arr = []
-        
-        self.preOrder(root, self.arr)
-        
-        obj={}
-        
-        for i in range(0, len(self.arr)):
-            rem = k-self.arr[i]
-            
-            if rem in obj:
+        left= Iterator(root, True)
+        right= Iterator(root, False)
+
+        i=left.next()
+        j=right.next()
+
+        while i<j:
+            if i+j==k:
                 return True
+            elif i+j>k:
+                j=right.next()
             else:
-                obj[self.arr[i]] = 1
+                i=left.next()
         return False
-    
-    def preOrder(self, node, arr):
+
         
-        if not node:
-            return None
-        
-        arr.append(node.val)
-        self.preOrder(node.left, arr)
-        self.preOrder(node.right, arr)
-    
-    
