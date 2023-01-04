5b93620eae9123cf6c316ed7828b877019f630c8
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2023-01-04 02:43:44 +0000

    Sync LeetCode submission - Longest Palindromic Substring (python3)

--- problems/longest_palindromic_substring/solution.py
+++ problems/longest_palindromic_substring/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ans=""
         n=len(s)
+        ans=""
         for i in range(0, n):
-            temp=self.isValid(s, i, i)
+            temp = self.valid(i, i, s)
             if len(temp)>len(ans):
                 ans=temp
             
-            temp=self.isValid(s, i, i+1)
-            if len(temp)>len(ans):
+            temp = self.valid(i, i+1, s)
+            if len(temp) > len(ans):
                 ans=temp
-
+        
         return ans
-    def isValid(self, s, l, r):
+    def valid(self, l, r, s):
         while l>=0 and r<len(s) and s[l]==s[r]:
             l-=1
             r+=1
