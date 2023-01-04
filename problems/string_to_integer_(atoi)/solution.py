fea41ee388c36272c3b5a7f72a7c92ac15316a44
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2023-01-04 03:33:59 +0000

    Sync LeetCode submission - String to Integer (atoi) (python3)

--- problems/string_to_integer_(atoi)/solution.py
+++ problems/string_to_integer_(atoi)/solution.py
@@ -1,11 +1,29 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()+" "
-        sign = -1 if s[0] == "-" else 1
-        p1 = p2 = 1 if(s[0] in ("-", "+")) else 0
-        while p2<len(s) and s[p2].isdigit(): p2+=1
-        num = sign * int(s[p1 : p2]) if(p2>p1) else 0
         
-        if(num> 0x7FFFFFFF): return 2147483647
-        if(num < -0x7FFFFFFF): return -2147483648
-        return num
+        MAX_NUM = 2 ** 31 - 1
+        MIN_NUM = -2 ** 31
+        
+        # trim the leading white space first
+        s = s.strip()
+        sign = 1
+        index = 0
+        num = 0
+        if not s:
+            return num
+        
+        if s[0] == '-':
+            sign = -1
+            index += 1
+        elif s[0] == '+':
+            index += 1
+        
+        while index < len(s) and s[index].isdigit():
+            curr_digit = ord(s[index]) - ord('0')
+            if num > MAX_NUM // 10 or (num == MAX_NUM // 10 and curr_digit > 7): # here we do the check before adding current digit
+                return MAX_NUM if sign == 1 else MIN_NUM
+            num = num * 10 + curr_digit
+            index += 1
+        
+        num = sign * num
+        return num 
