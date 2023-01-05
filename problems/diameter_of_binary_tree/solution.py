a7dd6a915c22dd16105b08dc69695476accb5e08
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2023-01-05 02:20:58 +0000

    Sync LeetCode submission - Diameter of Binary Tree (python3)

--- problems/diameter_of_binary_tree/solution.py
+++ problems/diameter_of_binary_tree/solution.py
@@ -7,24 +7,18 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.dia=0
-        def findHeight(node):
-            if not node:
-                return 0
-            
-            l=findHeight(node.left)
-            r=findHeight(node.right)
-            return 1+max(l,r)
-
-        def dfs(node):
-            if not node:
-                return
-            
-            lh=findHeight(node.left)
-            rh=findHeight(node.right)
-            self.dia=max(self.dia, lh+rh)
-
-            dfs(node.left)
-            dfs(node.right)
-
-        dfs(root)
+        self.findDiameter(root)
         return self.dia
+
+    
+    def findDiameter(self, node):
+        if not node:
+            return 0
+
+        lh = self.findDiameter(node.left)
+        rh = self.findDiameter(node.right)
+
+        self.dia = max(self.dia, lh+rh)
+
+        return 1+max(lh,rh)
+
