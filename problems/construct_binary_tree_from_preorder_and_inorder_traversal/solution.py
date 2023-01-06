b9dd9924ab6c7bcddea94c03a6c0ac815a9a51f1
Sourab Pramanik <61370770+sourabpramanik@users.noreply.github.com>
2023-01-06 01:18:17 +0000

    Sync LeetCode submission - Construct Binary Tree from Preorder and Inorder Traversal (python3)

--- problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
+++ problems/construct_binary_tree_from_preorder_and_inorder_traversal/solution.py
@@ -6,19 +6,21 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        nodes = dict()
+        store={}
         for i in range(0, len(inorder)):
-            nodes[inorder[i]] = i
-        return self.build(nodes, preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1)
-    
-    def build(self, nodes, preorder, preStart, preEnd, inorder, iStart, iEnd):
-        if preStart>preEnd or iStart>iEnd:
+            store[inorder[i]]=i
+        
+        return self.build(preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1, store)
+
+    def build(self, preorder, prs, pre, inorder, ins, ine, store):
+        if prs>pre or ins>ine:
             return None
         
-        val=preorder[preStart]
-        x=nodes[val]
-        nEle=x-iStart
+        val = preorder[prs]
+        x = store[val]
+        numLeft=x-ins
+
         root=TreeNode(val)
-        root.left= self.build(nodes, preorder, preStart+1, preStart+nEle, inorder, iStart, x-1)
-        root.right= self.build(nodes, preorder, preStart+nEle+1, preEnd, inorder, x+1, iEnd)
+        root.left=self.build(preorder, prs+1, prs+numLeft, inorder, ins, x-1, store)
+        root.right=self.build(preorder, prs+numLeft+1, pre, inorder, x+1, ine, store)
         return root
